7fb5dbb0a094c7e06a0d4fffaaaf335bfdaff32d
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-12-01 19:03:38 +0000

    assets

--- js/menuScene.js
+++ js/menuScene.js
@@ -18,8 +18,8 @@ class MenuScene extends Phaser.Scene {
 
   preload() {
     console.log("Menu Scene")
-    this.load.image("menuSceneBackground", "./assests/aliens_screen_image2.jpg")
-    this.load.image("startButton", "./assests/start.png")
+    this.load.image("menuSceneBackground", "./assets/aliens_screen_image2.jpg")
+    this.load.image("startButton", "./assets/start.png")
   }
 
   create(data) {
