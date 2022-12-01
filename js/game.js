4289235c4b4914d63822a588b40ac2d5ea84e972
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2022-12-01 02:54:11 +0000

    . / update

--- js/game.js
+++ js/game.js
@@ -8,7 +8,7 @@
 import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
 import MenuScene from "./menuScene.js"
-import GameScene from "/.gameScene.js"
+import GameScene from "./gameScene.js"
 
 // Our game scene
 const splashScene = new SplashScene()
