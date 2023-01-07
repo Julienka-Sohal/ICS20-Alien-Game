a92ff914fb5e737bab1f70c3fc4f1a698b6893b1
Julienka-Sohal <113055709+Julienka-Sohal@users.noreply.github.com>
2023-01-07 17:57:02 +0000

    explosions

--- js/gameScene.js
+++ js/gameScene.js
@@ -30,20 +30,36 @@ class GameScene extends Phaser.Scene {
 
   preload() {
     console.log("Game Scene")
+    // Image
     this.load.image("startBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
     this.load.image("missile", "assets/missile.png")
     this.load.image('alien', 'assets/alien.png')
+    // Sound 
     this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
   }
 
   create(data) {
     this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    // Missile Group
     this.missileGroup = this.physics.add.group()
+
+    // Alien Group
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    // collisions between missile and alien ( Explosions )
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
 
   update(time, delta) {
